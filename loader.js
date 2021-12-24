b5fae5f6aec543be23bd315c2a8f6671f23eadee
klein <39877504+uu-klein@users.noreply.github.com>
2021-12-24 19:05:05 +0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,51 +1,71 @@
+
 const translate = require("@vitalets/google-translate-api");
+
+class AsyncSeriesHook {
+  constructor(val) {
+    this.tasks = [];
+    this.val = val;
+    this.res = "";
+  }
+  promise(...args) {
+    let [o, ...funs] = this.tasks;
+    return funs.reduce((a, b) => a.then(() => b(...args)), o(...args));
+  }
+  tapPromise(task) {
+    this.tasks.push(task);
+  }
+}
 
 module.exports = function(source) {
   const callback = this.async();
 
-  const compose = (...fun) =>
-    fun.reduceRight((a, b) => (...args) => a(b(...args)));
+  let fn = new AsyncSeriesHook(source);
 
-  const matchFn = val => {
-    let reg = /[\u4e00-\u9fa5]+/g;
+  fn.tapPromise(val => {
+    return new Promise(resolve => {
+      let reg = /[\u4e00-\u9fa5]+/g;
+      let match = val.match(reg);
+      if (match !== null && match !== "null") {
+        let str = match.join(",");
+        fn.val = str;
+        resolve();
+      }
+    });
+  });
 
-    let match = val.match(reg);
-
-    if (match !== null && match !== "null") {
-      let str = match.join(",");
-      return str;
-    }
-  };
-
-  const translateFn = val => {
-    if (val !== null) {
-      translate(val, {
-        to: "vi"
-      })
-        .then(res => {
-          let regRes = /\'|'|'/g;
-
-          let newRes = res.text.replace(regRes, "").split(/, */);
-
-          let newArr = val.split(/, */);
-
-          for (let index = 0; index < newArr.length; index++) {
-            let res = `${newRes[index]}`;
-
-            let reg = new RegExp(`${newArr[index]}`, "g");
-
-            source = source.replace(reg, res);
-          }
-          callback(null, source);
+  fn.tapPromise(() => {
+    return new Promise(resolve => {
+      const { val } = fn;
+      if (val !== null) {
+        translate(val, {
+          to: "vi"
         })
-        .catch(err => {
-          console.error(err);
-        });
-    }
-  };
+          .then(res => {
+            fn.res = res;
+            resolve();
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      }
+    });
+  });
 
-  const fn = compose(matchFn, translateFn);
+  fn.tapPromise(source => {
+    const { res, val } = fn;
+    return new Promise(resolve => {
+      let regRes = /\'|'|'/g;
+      let newRes = res.text.replace(regRes, "").split(/, */);
+      let newArr = val.split(/, */);
+      for (let index = 0; index < newArr.length; index++) {
+        let res = `${newRes[index]}`;
+        let reg = new RegExp(`${newArr[index]}`, "g");
+        source = source.replace(reg, res);
+      }
+      callback(null, source);
+      resolve();
+    });
+  });
 
-  fn(source);
-
+  fn.promise(source);
 };
